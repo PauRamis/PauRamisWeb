4f227399f81a7f019d1c26b9f414645ae3226a2c
Your Name <you@example.com>
2025-06-02 11:05:32 +0200

    nav component

---
+++ navbar-component.js
@@ -0,0 +1,13 @@
+class NavBar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <ul class="navbar">
+        <li><a class="link" href="index.html">Inici</a></li>
+        <li><a class="link" href="projectes.html">Projectes</a></li>
+        <li><a class="link" href="curriculum.html">Curriculum</a></li>
+        <li><a class="link" href="contactes.html">Contactes</a></li>
+      </ul>
+    `;
+  }
+}
+customElements.define('nav-bar', NavBar);
